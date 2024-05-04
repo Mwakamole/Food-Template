3bc20d59c61bb4ce89fb9bd756d49c730a91eda4
Daniel <mwakamoledaniel@gmail.com>
2024-05-04 16:31:04 +0300

    Complete Menu

---
+++ index.js
@@ -0,0 +1,6 @@
+function w3_close() {
+    document.getElementsByClassName("menu_items")[0].style.display = "none";
+  }
+function w3_open() {
+    document.getElementsByClassName("menu_items")[0].style.display = "block";
+}
